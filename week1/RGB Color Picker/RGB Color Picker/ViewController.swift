baac72722ae50b337666b29b6e9238eef4f9b3a8
Emilio <emrod321@gmail.com>
2020-05-29 18:30:17 -0500

    Values update when sliders change

--- week1/RGB Color Picker/RGB Color Picker/ViewController.swift
+++ week1/RGB Color Picker/RGB Color Picker/ViewController.swift
@@ -11,27 +11,40 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var colorLabel: UILabel!
-    
     @IBOutlet weak var redSlider: UISlider!
     @IBOutlet weak var redLabel: UILabel!
     @IBOutlet weak var greenSlider: UISlider!
-    @IBOutlet weak var greenLabel: UISlider!
+    @IBOutlet weak var greenLabel: UILabel!
     @IBOutlet weak var blueSlider: UISlider!
     @IBOutlet weak var blueLabel: UILabel!
+    
+    // Initialize values for each slider
+    var redValue = 0,
+        blueValue = 0,
+        greenValue = 0
+    
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    
 
     @IBAction func redSliderChanged(_ sender: UISlider) {
+        redValue = Int(sender.value)
+        redLabel.text = String(redValue)
     }
     
     @IBAction func greenSliderChanged(_ sender: UISlider) {
+        greenValue = Int(sender.value)
+        greenLabel.text = String(greenValue)
     }
     
     @IBAction func blueSliderChanged(_ sender: UISlider) {
+        blueValue = Int(sender.value)
+        blueLabel.text = String(blueValue)
     }
     
     @IBAction func resetPressed(_ sender: UIButton) {
