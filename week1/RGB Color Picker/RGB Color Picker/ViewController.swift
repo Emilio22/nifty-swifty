ea7de3e35faca17d7e681de1311ea5dcbeb40514
Emilio <emiliorodriguez@Emilios-MacBook-Pro.local>
2020-05-29 10:53:50 -0500

    created project

---
+++ week1/RGB Color Picker/RGB Color Picker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RGB Color Picker
+//
+//  Created by Emilio Rodriguez on 5/29/20.
+//  Copyright © 2020 RWbc Emilio. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
