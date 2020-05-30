8407b604f024d4071ec6a2a0aede879e794c7097
Emilio <emrod321@gmail.com>
2020-05-29 23:32:45 -0500

    added reset functionality

--- week1/RGB Color Picker/RGB Color Picker/ViewController.swift
+++ week1/RGB Color Picker/RGB Color Picker/ViewController.swift
@@ -28,10 +28,8 @@ class ViewController: UIViewController {
         blueValue : Float = 0.0,
         greenValue : Float = 0.0
     
-    var colorName : String = ""
-    
     //Constant to determin how bright the background is
-    let tooBright : Float = 600.00
+    let tooBright : Float = 475.00
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -44,21 +42,32 @@ class ViewController: UIViewController {
 
     @IBAction func redSliderChanged(_ sender: UISlider) {
         redValue = sender.value.rounded()
-        redLabel.text = String(redValue)
+        updateLabels()
+        updateColors()
     }
     
     @IBAction func greenSliderChanged(_ sender: UISlider) {
         greenValue = sender.value.rounded()
-        greenLabel.text = String(greenValue)
+        updateLabels()
+        updateColors()
     }
     
     @IBAction func blueSliderChanged(_ sender: UISlider) {
         blueValue = sender.value.rounded()
-        blueLabel.text = String(blueValue)
+        updateLabels()
+        updateColors()
     }
     
     @IBAction func resetPressed(_ sender: UIButton) {
-        
+        blueValue = 0
+        redValue = 0
+        greenValue = 0
+        blueSlider.value = 0
+        redSlider.value = 0
+        greenSlider.value = 0
+        updateLabels()
+        updateColors()
+        colorLabel.text = "Pick a Color"
     }
     
     @IBAction func setColorPressed(_ sender: UIButton) {
@@ -67,7 +76,9 @@ class ViewController: UIViewController {
         alertController.addTextField()
         let action = UIAlertAction(title: "Colorize", style: .default, handler:{
             action in
-            self.colorName = alertController.textFields![0].text!
+            
+            self.colorLabel.text = alertController.textFields![0].text
+            
             self.updateColors()
         })
         
@@ -76,19 +87,41 @@ class ViewController: UIViewController {
         
     }
     
+    func updateLabels() {
+        blueLabel.text = String(blueValue)
+        redLabel.text = String(redValue)
+        greenLabel.text = String(greenValue)
+    }
+    
     func updateColors(){
         let cgRed = CGFloat(redValue / redSlider.maximumValue)
         let cgGreen = CGFloat(greenValue / greenSlider.maximumValue)
         let cgBlue = CGFloat(blueValue / blueSlider.maximumValue)
         let cgAlpha = CGFloat(1.0)
         let rgbSum = redValue + greenValue + blueValue
-        colorLabel.text = colorName
+        print(rgbSum)
         
         //if background is bright, change font to be dark
-        if rgbSum > tooBright{
+        if rgbSum > tooBright {
+            colorLabel.textColor = UIColor.black
+            redLabel.textColor = UIColor.black
+            greenLabel.textColor = UIColor.black
+            blueLabel.textColor = UIColor.black
+            rLabel.textColor = UIColor.black
+            gLabel.textColor = UIColor.black
+            bLabel.textColor = UIColor.black
             
+        } else {
+            colorLabel.textColor = UIColor.white
+            redLabel.textColor = UIColor.white
+            greenLabel.textColor = UIColor.white
+            blueLabel.textColor = UIColor.white
+            rLabel.textColor = UIColor.white
+            gLabel.textColor = UIColor.white
+            bLabel.textColor = UIColor.white
         }
         
+        //Change background
         self.view.backgroundColor = UIColor.init(displayP3Red: cgRed, green: cgGreen, blue: cgBlue, alpha: cgAlpha)
     }
     
