9fb3a52d25fedc5713bc4c51da2032bb33742f8b
Emilio <emrod321@gmail.com>
2020-06-07 22:23:34 -0500

    Started project, added Model and Controller folder

---
+++ week2/BullsEye/BullsEye/Model/BullsEyeGame.swift
@@ -0,0 +1,15 @@
+//
+//  BullsEyeGame.swift
+//  BullsEye
+//
+//  Created by Emilio Rodriguez on 6/7/20.
+//  Copyright © 2020 Ray Wenderlich. All rights reserved.
+//
+
+import Foundation
+
+struct BullsEyeGame {
+    
+    
+    
+}
