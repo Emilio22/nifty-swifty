97ad2ae9c5758d7161c5a976ee3ac03012d9e14d
Emilio <emrod321@gmail.com>
2020-06-08 00:11:48 -0500

    added Model/BullsEyeGame and refactored code to implement MVC

--- week2/BullsEye/BullsEye/Model/BullsEyeGame.swift
+++ week2/BullsEye/BullsEye/Model/BullsEyeGame.swift
@@ -10,6 +10,53 @@ import Foundation
 
 struct BullsEyeGame {
     
+    var currentValue = 0
+    var targetValue = 0
+    var score = 0
+    var round = 0
+    
+    var title : String?
+    var message : String?
+    
+    var difference: Int {
+        return abs(targetValue - currentValue)
+    }
+   
+    
+    mutating func startNewGame(){
+        score = 0
+        round = 0
+        startNewRound()
+    }
+    
+    mutating func startNewRound(){
+        round += 1
+        targetValue = Int.random(in: 1...100)
+        currentValue = 50
+    }
+    
+    mutating func calculatePoints(){
+        
+        var points = 100 - difference
+        
+        if difference == 0 {
+          title = "Perfect!"
+          points += 100
+        } else if difference < 5 {
+          title = "You almost had it!"
+          if difference == 1 {
+            points += 50
+          }
+        } else if difference < 10 {
+          title = "Pretty good!"
+        } else {
+          title = "Not even close..."
+        }
+        
+        message = "You scored \(points) points"
+        score += points
+        
+    }
     
     
 }
